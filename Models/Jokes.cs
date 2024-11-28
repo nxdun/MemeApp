ae67464c09d67759901bca541ffb38a15ed08d75
Nadu_Dev <inbox.nadun@gmail.com>
2024-11-28 17:46:14 +0530

    Add project files.

---
+++ Models/Jokes.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string jokeQ { get; set; }
+        public string jokeA{ get; set; }
+
+        public Jokes()
+        {
+            
+        }
+    }
+}
